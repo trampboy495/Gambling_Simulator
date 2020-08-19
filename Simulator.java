360e694f2c697d9d91ad90367d481d3bef73f2e1
trampboy495 <histroylover@gmail.com>
2020-08-19 18:29:40 +0530

    [siddhesh]add.Uc1 added bet and stake amount

---
+++ Simulator.java
@@ -0,0 +1,11 @@
+public class Simulator{
+	 final int Stake;
+         final int Bet;
+	public Simulator(){
+		this.Stake=100;
+		this.Bet=1;
+	}
+	public static void main(String[]args) {
+		new Simulator();
+	}
+}
